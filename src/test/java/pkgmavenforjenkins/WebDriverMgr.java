2af804bfc112d813c2ba6a79b73d139b1cc9761e
Iroshan Bandara <iroshan4u@gmail.com>
2023-12-24 17:18:23 +0100

    added paramater

--- src/test/java/pkgmavenforjenkins/WebDriverMgr.java
+++ src/test/java/pkgmavenforjenkins/WebDriverMgr.java
@@ -2,22 +2,33 @@ package pkgmavenforjenkins;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class WebDriverMgr {
+	@Parameters("Browser")
 	@Test
-	public void StartBrowser() {
+	public void StartBrowser(String browsserName) {
 
-		// WebDriverManager.chromedriver().setup();
-		// WebDriver driver = new ChromeDriver();
+		WebDriver driver = null;
+		System.out.println("Parameter value is: " + browsserName);
 
-		WebDriverManager.firefoxdriver().setup();
-		WebDriver driver = new FirefoxDriver();
+		if (browsserName.contains("Chrome")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
 
+		} else if (browsserName.contains("Edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
+
+		driver.manage().window().maximize();
 		driver.get("https://google.com");
 		driver.quit();
 	}
+
 }
